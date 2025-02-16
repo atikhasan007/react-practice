182dca8d8626af2ff9e6983e65611e2fbb6aaca2
atik007 <atikhasan1971arik@gmail.com>
2025-02-16 16:40:52 +0600

    The <Mouse> component encapsulates the behavior we need.

--- src/components/MouseTracker.jsx
+++ src/components/MouseTracker.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-
+import Mouse from "./Mouse";
 export default class MouseTracker extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleMouseMove = this.handleMouseMove.bind(this);
-        this.state = {
-            x : 0,
-            y : 0,
-        }
-    }
+ 
 
 
-
-    handleMouseMove(event){
-        this.setState({
-            x : event.clientX,
-            y: event.clientY
-        })
-    }
     render() {
       return (
-        <div style={{height:'100vh'} } onMouseMove={this.handleMouseMove}>
+        <div>
             <h1>Move the mouse around!</h1>
-            <p>The Current mouse poisiton is ({this.state.x}, {this.state.y}) </p>
+            <Mouse />
+
+          
           
         </div>
       )
