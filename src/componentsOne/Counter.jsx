7971dcad6452733a2170a594c6efc78397154fea
atik007 <atikhasan1971arik@gmail.com>
2025-02-17 23:56:57 +0600

    useState with previous state problem

--- src/componentsOne/Counter.jsx
+++ src/componentsOne/Counter.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 
 function Counter(){
   const [count , setCount ] = useState(0);
+
+  let i = 0;
+  const addFive = () =>{
+    while(i<5){
+        setCount(count + 1);
+        i=i+1;
+    }
+  }
+
     return (
         <div>
             {count }<p>
@@ -13,6 +22,13 @@ function Counter(){
                 )
             }>Add 1</button>
 
+            </p>
+
+
+            <hr/>
+
+            <p>
+                <button type="button"  onClick={addFive}>add 5</button>
             </p>
            
 
