1d39bef4e8844990a3fd6c4060180d3bf1030328
atik007 <atikhasan1971arik@gmail.com>
2025-02-17 23:53:14 +0600

    useState with previous state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 
 import React from "react";
-import Todo from "./componentsOne/Todo";
+import Counter from "./componentsOne/Counter";
 
 
 export default class App extends React.Component {
     render(){
         return (
             <div>
-            <Todo />
+            <Counter />
             </div>
         )
     }
