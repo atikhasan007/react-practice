e7354fdcfcf7bc9cc5cc929bc5190b6e76ec440e
atik007 <atikhasan1971arik@gmail.com>
2025-02-17 23:31:07 +0600

    ---spread operator useing object useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 
 import React from "react";
+import Todo from "./componentsOne/Todo";
 
-import Todo from './componentsOne/TodoClass';
 
 export default class App extends React.Component {
     render(){
         return (
             <div>
-                <Todo />
+            <Todo />
             </div>
         )
     }
--- src/componentsOne/Todo.jsx
+++ src/componentsOne/Todo.jsx
@@ -2,36 +2,42 @@ import { useState } from "react";
 
 function Todo(){
    
-    const [todo, setTodo] = useState('');
-    const [warning, setWarning] = useState('');
+    const [todo, setTodo] = useState({
+        title: '',
+        description: ''
+    });
 
-
-   const handleInput = (e) =>{
-        const inputValue = e.target.value;
-        const updateWarning = inputValue.includes('.js')
-        ? 'You need javaScript skill to complete the task. Do you have it?'
-        : null;
-      
-
-        setTodo(inputValue);
-        setWarning(updateWarning);
-
-       
-    }
-
+    const {title, description} = todo;
+    
     return (
         <div>
-            <p>{todo}</p>
+            <p>{title}</p>
             <p>
-                <textarea name = 'todo'  value={todo} onChange={handleInput}/>
+                <input type="text" value={title} 
+                onChange={(e)=>{
+                    setTodo ({
+                        ...todo,
+                        title : e.target.value,
+                    })
+                }}
+                />
+            </p>
+            <p>
+                <textarea name = 'todo'  value={description} onChange={(e)=>{
+                    setTodo({
+                        ...todo,
+                        description : e.target.value
+                    })
+                }}/>
             </p>
 
             <hr/>
-            <h2>{warning || 'Good choice'}</h2>
+
           
         </div>
       )
 
 }
+
 
 export default Todo;
