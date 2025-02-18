d079a37d036454faf84c8db1a01ae32dd4712e9b
atik007 <atikhasan1971arik@gmail.com>
2025-02-19 02:54:03 +0600

    usecallback useing and problem solve when todo functon is not state or props change then render hoy ei pbl solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 
 
-import React from "react";
-import Counter from "./MyPractice/UseEffect/Counter";
+import React, { useCallback, useState } from "react";
+import Todos from "./MyPractice/UseCallbackAndUseMemo/Todos";
 export default function App() {
 
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState([]);
+
+  const increment = () =>{
+    setCount((c)=>c+1);
+  }
+     
+    const addTodo = useCallback(() =>{
+        setTodos((t)=> [...t, "New Todo"]);
+    },[todos])
+
     return (
-     <div>
-        <Counter/>
-        
+        <div> 
+     <Todos  todos={todos} addTodo={addTodo}/>
+
+     <hr/>
+
+        <div>
+            Count : {count}
+            <br/>
+            <button onClick={increment}>+</button>
+        </div>
+     
      </div>
     )
 
