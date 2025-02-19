b9cc545d6b6178d071f22b4415fe1c0064ae2005
atik007 <atikhasan1971arik@gmail.com>
2025-02-19 23:46:53 +0600

    state and action as object

--- src/contexts/complexCounter.jsx
+++ src/contexts/complexCounter.jsx
@@ -3,15 +3,23 @@ import React, { useReducer } from 'react';
 
 const  initialState = {
     counter:0,
+    counter2: 0,
 
 }
 
 const reducer = (state, action) =>{
       switch(action.type){
         case  'increment':
-            return  {counter : state.counter + action.value};
+            return  {...state , counter : state.counter + action.value};
         case 'decrement':
-            return {counter: state.counter - action.value};
+            return {...state ,counter: state.counter - action.value};
+
+
+            case  'increment2':
+            return  {...state, counter2 : state.counter2 + action.value};
+        case 'decrement2':
+            return {...state, counter2: state.counter2 - action.value};
+
 
         default:
             return state;
@@ -25,6 +33,34 @@ const  ComplexCounter = () => {
  
 
   return (
+<>
+    <div>
+    <div>Count2 - {count.counter2}</div>
+    <button type='button' onClick={()=>dispatch({
+        type : 'increment2',
+        value : 1
+    })}>Increment2 by 1</button>
+
+
+
+    <button type='button' onClick={()=>dispatch({
+        type : 'decrement2',
+        value: 1
+    })}>Decrement2 by 1 </button>
+  
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
 
     <div>
         <div>Count - {count.counter}</div>
@@ -32,11 +68,6 @@ const  ComplexCounter = () => {
             type : 'increment',
             value : 1
         })}>Increment by 1</button>
-
-<button type='button' onClick={()=>dispatch({
-            type : 'increment',
-            value : 5
-        })}>Increment by 5</button>
 
 
 
@@ -46,11 +77,8 @@ const  ComplexCounter = () => {
         })}>Decrement by 1 </button>
       
 
-      <button type='button' onClick={()=>dispatch({
-            type : 'decrement',
-            value: 5
-        })}>Decrement by 5 </button>
     </div>
+    </>
   )
 }
 
