25b43bdf1087c36f41480032eed0b88a9edf76db
atik007 <atikhasan1971arik@gmail.com>
2025-02-21 18:43:54 +0600

    style css local and global

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 
-import React from 'react'
-import LayoutComponentOne from './customHooks/LayoutComponentOne'
-import LayoutComponentTwo from './customHooks/LayoutComponentTwo'
+import React from 'react';
+import Logo from '../public/style/Logo';
+import './assets/css/global.css';
+
 
 
 export default function App() {
   return (
     <div className='App'>
-        
-        <LayoutComponentTwo/>
-        <LayoutComponentOne/>
+        <h1>Learn with Atik</h1>
+        <Logo/>
+     
     </div>
   )
 }
